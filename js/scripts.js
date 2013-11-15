574452114da6e76831c21b70fad4d9aaa8d53e72
Ed Hebert <ehebert@fas.harvard.edu>
2013-11-15 13:51:15 -0500

    added mouse tracking behavior

--- js/scripts.js
+++ js/scripts.js
@@ -1,173 +1,167 @@
-var mover;
+/*
 
-var moverStyle = {
-    fillColor:		"#FB4C9F",
-	strokeColor: 	"#CA2A65",
-	strokeJoin:		"round",
-	strokeWidth:	2
-};
+Ed Hebert
+ehebert@fas.harvard.edu
+DWA15 - Project 3 - Javascript
+
+This project uses the Paper.js library to create a natural organic object.
+It will wander randomly, respond to the environment, etc.
+
+The concepts for this project are adapted from Chapter 6 of the book 
+"The Nature of Code", By Dan Shiffman. It discusses the creation of "autonomous 
+agents" with 
+
+His book details algorithms and code in the Processing language.
+His concepts for organic movement have been ported to Javascript / Paper.js, 
+and applied to my own object.
+
+http://natureofcode.com
+http://paperjs.org
+
+*/
 
 
-// Paperjs direct mode settings
-paper.install( window );
+// the global creature, called "vehicle"
+var vehicle;
+// Access paper.js directly through JavaScript
+paper.install(window);
 
 
 window.onload = function() {
-	paper.setup( "myCanvas" );
-	
-	// Create jellies
-		mover = new Mover();
-		mover.init();
+    paper.setup("myCanvas");
+    
+    // Create a vehicle object
+    vehicle = new Vehicle();
+    vehicle.init();
 
-	
-	// Set drawing loop
-	view.onFrame = draw;
+    // initialize the mouse tool in Paper.js
+    tool = new Tool();
 
+    // initialize mouse Position
+    var mousePosition = new Point (-100, -100);
 
+    // Set drawing loop
+    view.onFrame = function(event) {
+        vehicle.update();
+        vehicle.checkBoundaries();
+        vehicle.seek(mousePosition);         
+    }
+
+    // Define a mousedown and mousedrag handler
+    tool.onMouseMove = function(event) {
+        mousePosition = new Point(event.point);    
+    }
 };
 
 
-function draw( evt ) {
-	mover.wander();
-	mover.update( evt );
-	mover.checkBounds();	
+/* Begin Vehicle Class */
+
+function Vehicle() {
+    // the path that draws the vehicle
+    this.path           = new Path();
+
+    // the vectors that will govern the vehicle's motion (load from off screen)
+    this.location       = new Point(-100 , Math.random() * (view.size.height * 0.5));
+    this.velocity       = new Point(0, 0);
+    this.acceleration   = new Point(0, 1);
+
+    // the maximum desired speed of the vehicle   
+    var maxSpeed        = 10; // Math.random() * 0.2 + 1;
+    console.log(maxSpeed);
+
+    // the magnitude of its steering ability
+    var maxForce        = .8;
+    
+    // established the heading / direction of the vehicle
+    var angle           = (Math.PI * 2);
+    var wrapAngle       = (Math.PI / 2) + angle;
+    var wanderTheta     = 0;
+    
+    var orientation     = 0;
+    var lastOrientation = 0;
+    var lastLocation;
+        
+    
+    this.init = function() {
+        // construct the vehicle shape
+        this.path.strokeColor = 'black';
+        this.path.add(new Point(0, 90));
+        this.path.add(new Point(40, 0));
+        this.path.add(new Point(80, 90));
+
+        this.path.closed = true;        
+    };
+
+
+    // function to apply various force vectors to vehicle acceleration
+    this.applyForce = function(force) {
+        this.acceleration = this.acceleration.add(force);
+    }
+ 
+
+     // wraps the vehicle object to the opposite side of the screen    
+    this.checkBoundaries = function() {
+
+        // create offset of 'white space' beyond the window
+        var offset = 50;
+        if (this.location.x < -offset) {
+            this.location.x = view.size.width + offset;
+        }
+        if (this.location.x > view.size.width + offset) {
+            this.location.x = -offset;
+        }
+        if (this.location.y < -offset) {
+            this.location.y = view.size.height + offset;
+        }
+        if (this.location.y > view.size.height + offset) {
+            this.location.y = -offset;
+        }
+    }
+
+
+    this.seek = function(target) {
+        // scale desired speed according to max speed
+        var desired = new Point(target.subtract(this.location));
+        desired = desired.normalize();
+        desired = desired.multiply(maxSpeed);
+
+        // create a steering Force
+        var steer = new Point(desired.subtract(this.velocity));
+
+        // limit the magnitude of the steering force
+        steer.length = Math.min( maxForce, steer.length );
+
+        // apply steering force to our acceleration
+        this.applyForce(steer);
+    }
+
+    
+    this.update = function() {
+        lastLocation = this.location.clone();
+        
+        // velocity = velocity + acceleration
+        this.velocity = this.velocity.add(this.acceleration);
+
+        // regulate the velocity to the max speed
+        this.velocity.length = Math.min(maxSpeed, this.velocity.length);
+        
+        // location = location + velocity
+        this.location = this.location.add(this.velocity);
+        
+        // reset acceleration
+        this.acceleration.length = 0;
+        
+        // Change vehicle path position, without this it won't move
+        this.path.position = this.location.clone();
+        
+        // Align rotation to match direction of velocity vector
+        var theta = new Point(this.location.subtract(lastLocation));
+        orientation = theta.angle + 90;
+        this.path.rotate(orientation - lastOrientation);
+        lastOrientation = orientation;
+    };   
 }
 
-
-// -------------------------------------
-// ---- mover Class BEGIN
-
-function Mover() {
-	this.path 			= new Path();
-	this.location		= new Point( -100 , Math.random() * (view.size.height * 0.5) );
-	this.velocity		= new Point( 0, 0 );
-	this.acceleration	= new Point( 0, 0 );
-
-	// the maximum speed of the moverfish	
-	var maxSpeed		= Math.random() * 0.2 + 1;
-	// the magnitude of its steering force
-	var maxForce		= 0.4;
-	
-	// established the heading / direction of the mover
-	var angle 			= (Math.PI * 2);
-	var wrapAngle 		= (Math.PI / 2) + angle;
-	var wanderTheta		= 0;
-	
-	var orientation		= 0;
-	var lastOrientation = 0;
-	var lastLocation;
-		
-	
-	this.init = function() {
-		// Creates body path
-
-		this.path.strokeColor = 'black';
-		this.path.add(new Point(0, 90));
-		this.path.add(new Point(40, 0));
-		this.path.add(new Point(80, 90));
-
-		this.path.closed = true;
-		
-	};
-	
-	
-	this.update = function( evt ) {
-		lastLocation = this.location.clone();
-		
-		this.velocity.x += this.acceleration.x;
-		this.velocity.y += this.acceleration.y;
-		console.log('this.acceleration: ' + this.acceleration);
-	    this.velocity.length = Math.min( maxSpeed, this.velocity.length );
-	    
-	    this.location.x += this.velocity.x;
-	    this.location.y += this.velocity.y;
-	    
-	    this.acceleration.length = 0;
-		
-		// Change mover path position, without this it won't move
-		this.path.position = this.location.clone();
-		
-		// Rotation alignment
-		var locVector = new Point( this.location.x - lastLocation.x, this.location.y - lastLocation.y );
-		orientation = locVector.angle + 90;
-		this.path.rotate( orientation - lastOrientation );
-		lastOrientation = orientation;
-	};
-	
-	
-	this.steer = function( target, slowdown ) {
-		var steer;
-		var desired	= new Point( target.x - this.location.x, target.y - this.location.y );
-		var dist 	= desired.length;
-		
-		if ( dist > 0 ) {
-			if ( slowdown && dist < 100 ) {
-				desired.length = maxSpeed * ( dist / 100 );
-			}
-			else {
-				desired.length = maxSpeed;
-			}
-			
-			steer = new Point( desired.x - this.velocity.x, desired.y - this.velocity.y );
-
-			// limit the steering ability by the maxForce
-			steer.length = Math.min( maxForce, steer.length );
-		}
-		else {
-			steer = new Point( 0, 0 );
-		}
-		return steer;
-	}
-	
-	
-	this.seek = function( target ) {
-		var steer = this.steer( target, false );
-		this.acceleration.x += steer.x;
-		this.acceleration.y += steer.y;
-	}
-	
-	
-	this.wander = function() {
-		var wanderR 	= 5;
-		var wanderD		= 100;
-		var change		= 0.05;
-		
-		wanderTheta += Math.random() * (change * 2) - change;
-		
-		var circleLocation = this.velocity.clone();
-		circleLocation = circleLocation.normalize();
-		circleLocation.x *= wanderD;
-		circleLocation.y *= wanderD;
-		circleLocation.x += this.location.x;
-		circleLocation.y += this.location.y;
-		
-		var circleOffset = new Point( wanderR * Math.cos( wanderTheta ), wanderR * Math.sin( wanderTheta ) );
-		
-		var target = new Point( circleLocation.x + circleOffset.x, circleLocation.y + circleOffset.y );
-		
-		this.seek( target );
-	}
-	
-	// wraps the mover object when it heads off screen	
-	this.checkBounds = function() {
-		var offset = 100;
-		if ( this.location.x < -offset ) {
-			this.location.x = view.size.width + offset;
-		}
-		if ( this.location.x > view.size.width + offset ) {
-			this.location.x = -offset;
-		}
-		if ( this.location.y < -offset ) {
-			this.location.y = view.size.height + offset;
-		}
-		if ( this.location.y > view.size.height + offset ) {
-			this.location.y = -offset;
-		}
-	}
-	
-}
-
-// ---- Mover Class END
-// -------------------------------------
+/* End Vehicle Class */
 
 
